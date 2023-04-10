070a1b1481af5e5b1bc7af7d06593af986abad1d
Dhrubhagat <dhrubhagatjhoond@gmail.com>
2023-04-09 22:07:13 -0400

    fixed CSV saving

--- build an ocr workflow/ocr.py
+++ build an ocr workflow/ocr.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pytesseract
 from PIL import Image
 import csv
-import base64
+import os
 
 def ocr_image(image, languages):
     """
@@ -45,17 +45,15 @@ def save_results(results):
     """
     Save the OCR results to a CSV file.
     """
+    print("Save results has run")
+    csv_path = "ocr_results.csv"
+    file_exists = os.path.isfile(csv_path)
     with open("ocr_results.csv", "w", newline="", encoding="utf-8") as f:
         writer = csv.writer(f)
-        writer.writerow(["File Name", "Extracted Text"])
+        if not file_exists:
+            writer.writerow(["File Name", "Extracted Text"])
+        #writer.writerow(["File Name", "Extracted Text"])
         writer.writerows(results)
-
-def download_csv(results):
-   
-    csv_data = "File Name,Extracted Text\n" + "\n".join([f"{name},{text}" for name, text in results])
-    b64_data = base64.b64encode(csv_data.encode()).decode()
-    href = f'<a href="data:file/csv;base64,{b64_data}" download="ocr_results.csv">Download CSV</a>'
-    st.markdown(href, unsafe_allow_html=True)
 
 
 # Set up the Streamlit app
@@ -68,17 +66,23 @@ images = st.file_uploader("Select image(s)", accept_multiple_files=True)
 languages = st.multiselect("Select language(s)", ["eng", "spa", "fra"])
 
 if st.button("Process Images"):
+    print('clicked')
     if images:
         # Perform OCR on the selected images
         results = process_images(images, languages)
 
         # Display the results in a table
         st.table(results)
-
-        # Save the results as a CSV file
-        if st.button("Save as CSV"):
-            save_results(results)
-            download_csv(results)
+        
 
     else:
         st.warning("Please select one or more images to process.")
+
+if st.button("Save as CSV"):
+    results = process_images(images, languages)
+    if results:
+        # Save the results as a CSV file in local directory
+        save_results(results)
+        
+    else:
+        st.warning("Please process images and extract text before saving as CSV.")
